40fc3b3f6f19bcdda91eb609d77126ce4d935b68
Daniel Proaño <daniel.p.g.ecu@gmail.com>
2022-12-29 23:16:48 +0000

    Rescue branch
    Commited externally. Might need to restart the repo

--- source/vertical_counter.v
+++ source/vertical_counter.v
@@ -6,17 +6,17 @@
 module vertical_counter(
        input 		 clk_25MHz,
        input 		 enable_V_counter,
-       output reg [15:0] V_Count_Value = 0,
+       output reg [15:0] V_Count_Value = 0
 			  );
 
    always@(posedge clk_25MHz) begin
 
-      if (enable_v_counter == 1'b1) begin
+      if (enable_V_counter == 1'b1) begin
 	 
 	 if (V_Count_Value < 524) //VGA standard
 	   V_Count_Value <= V_Count_Value + 1;
 	 else 
-	   V_Counter_Value <= 0; // Reset H counter
+	   V_Count_Value <= 0; // Reset H counter
       end
    end
 endmodule // vertical_counter
--- source/vga_620x480.v
+++ source/vga_620x480.v
@@ -16,7 +16,7 @@ module vga(
 	   output [3:0] Red,
 	   output [3:0] Green,
 	   output [3:0] Blue,
-	   output [31:0] pixel_ADDR
+	   output reg[31:0] pixel_ADDR
 	   );
 
    wire 		clk_25M;
@@ -29,11 +29,11 @@ module vga(
    // Module Instantiation
    clock_divider VGA_clk_gen (clk, clk_25M);
    horizontal_counter VGA_H_count (clk_25M, enable_V_counter, H_count_value);
-   vertical_counter VHA_V_count (clk_25M ,enable_V_counter, V_count_value);
+   vertical_counter VGA_V_count (clk_25M ,enable_V_counter, V_count_value);
 
    // Outputs
-   assign h_sync = (H_count_value < 96) ? 1'b0:1'b1;
-   assign v_sync = (V_count_value < 2) ? 1'b0:1'b1;
+   assign h_sync = (H_count_value < 96 || (H_count_value < 784 && H_count_value > 143)) ? 1'b1:1'b0;
+   assign v_sync = (V_count_value < 2 || (V_count_value < 515 && V_count_value > 34)) ? 1'b1:1'b0;
    // Colors
    assign Red = (H_count_value < 784 && H_count_value > 143 && V_count_value < 515 && V_count_value > 34) ? pixel[11:8]:4'h0;
    assign Green = (H_count_value < 784 && H_count_value > 143 && V_count_value < 515 && V_count_value > 34) ? pixel[7:4]:4'h0;
@@ -43,8 +43,8 @@ module vga(
       if (H_count_value < 784 && H_count_value > 143 && V_count_value < 515 && V_count_value > 34) begin
 	 ADDR_count <= ADDR_count + 1;
       end
-      if ADDR_count == 297599 begin
-	 ADDR_count <= 0;
+      if (ADDR_count == 297599) begin
+	 ADDR_count <= 32'h00000000;
       end
       pixel_ADDR <= ADDR_count << 4;
    end
